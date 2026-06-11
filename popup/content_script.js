010097e93b2e9b9f2766562c1f3c4db032a06964
Patryk Woźnicki <wp55805@zut.edu.pl>
2026-06-11 11:24:04 +0200

    feat: fetching questions by URL, UI improvements and search fixes

--- popup/content_script.js
+++ popup/content_script.js
@@ -2,37 +2,37 @@
 * Gets the HTML of the user's selection
 */
 	function getSelectionHTML() {
-	  var userSelection;
-	  if (window.getSelection) {
-		// W3C Ranges
-		userSelection = window.getSelection ();
-		// Get the range:
-		if (userSelection.getRangeAt)
-		  var range = userSelection.getRangeAt (0);
-		else {
-		  var range = document.createRange ();
-		  range.setStart (userSelection.anchorNode, userSelection.anchorOffset);
-		  range.setEnd (userSelection.focusNode, userSelection.focusOffset);
+		var userSelection;
+		if (window.getSelection) {
+			// W3C Ranges
+			userSelection = window.getSelection ();
+			// Get the range:
+			if (userSelection.getRangeAt)
+				var range = userSelection.getRangeAt (0);
+			else {
+				var range = document.createRange ();
+				range.setStart (userSelection.anchorNode, userSelection.anchorOffset);
+				range.setEnd (userSelection.focusNode, userSelection.focusOffset);
+			}
+			// And the HTML:
+			var clonedSelection = range.cloneContents ();
+			var div = document.createElement ('div');
+			div.appendChild (clonedSelection);
+			return div.innerHTML;
+		} else if (document.selection) {
+			// Explorer selection, return the HTML
+			userSelection = document.selection.createRange ();
+			return userSelection.htmlText;
+		} else {
+			return '';
 		}
-		// And the HTML:
-		var clonedSelection = range.cloneContents ();
-		var div = document.createElement ('div');
-		div.appendChild (clonedSelection);
-		return div.innerHTML;
-	  } else if (document.selection) {
-		// Explorer selection, return the HTML
-		userSelection = document.selection.createRange ();
-		return userSelection.htmlText;
-	  } else {
-		return '';
-	  }
 	}
 /**
 * Listens for a request from the button in the browser.
 * When it sees the getSelection request, it returns the selection HTML, as well as the URL and title of the tab.
 */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.method == "getSelection"){
+	if (request.method == "getSelection") {
 		try {
 			var selection = window.getSelectionHTML();
 		} catch (error) {
